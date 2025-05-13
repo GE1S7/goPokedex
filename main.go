52cfab01a456563596d44e74a46f778169da08a1
ge1s7 <kacperkalinowski@protonmail.com>
2025-05-13 17:50:52 +0200

    ADD command registry; exit command

--- main.go
+++ main.go
@@ -8,6 +8,26 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+// map of valid commands
+var commandRegistry = map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	},
+	"help": {
+		name:        "help",
+		description: "Display a help message",
+		callback:    commandHelp,
+	},
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for true {
@@ -29,16 +49,34 @@ func main() {
 			continue
 		}
 
-		fmt.Println("Your command was:", cleanInput(prompt)[0])
+		command, exists := commandRegistry[prompt]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println("Unknown command")
+			}
+		}
 
 	}
+}
+
+func commandHelp() error {
+	return nil
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
 }
 
 func cleanInput(text string) []string {
 	/* split on whitespace, lowercase
 	remove leading and trailing whitespace*/
 
-	v := strings.Split(strings.ToLower(strings.TrimSpace(text)), " ")
-	return v
+	clean := strings.Split(strings.ToLower(strings.TrimSpace(text)), " ")
+	return clean
 
 }
